671bfd5d29e6c84fef5aeacdd14c46064c9ff522
Mauricio Torres <otarksk@gmail.com>
2022-02-18 15:29:49 -0300

    Tres modelos creados de tablas

---
+++ src/main/java/com/example/crudultimo/Models/Employee.java
@@ -0,0 +1,109 @@
+package com.example.crudultimo.Models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+/**
+ * ---Role Declaramos role con las etiquetas @ManyToOne porque es de muchos a uno.
+ * Y JoinColumn para que cree automaticamente el campo de nombre id_role, esta sera la clave foranea
+ *
+ * --- Project es ManyToMany porque hay una dependencia bilateral de las tablas, y estas deben de crear
+ * una tercera tabla para establecer las relaciones
+ */
+
+
+@Entity
+public class Employee {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 25, nullable = false)
+    private String firstName;
+
+    @Column(length = 25, nullable = false)
+    private String lastName;
+
+    @Column(length = 10,nullable = false)
+    private String employeeid;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name="id_role")
+    private Role role;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "employee_project",
+            joinColumns = { @JoinColumn(name = "employee_id") },
+            inverseJoinColumns = { @JoinColumn(name = "project_id") })
+    private List<Project> projects = new ArrayList<Project>();
+
+    public Employee(){
+
+    }
+
+    public Employee(String firstName, String lastName, String employeeid){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeid = employeeid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(String employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id.equals(employee.id) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(employeeid, employee.employeeid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", employeeid='" + employeeid + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/crudultimo/Repository/IEmployeeJpaRepository.java
@@ -0,0 +1,19 @@
+package com.example.crudultimo.Repository;
+
+import com.example.crudultimo.Models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Interface que extiende de JpaRepository y este es un repositorio para una tabla de tipo
+ * Employee y su indentificado es de tipo Long
+ */
+
+@Repository
+public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
+    // TODO: 2/18/2022 Esto lo puedo hacer con todos los campos, tener en cuenta para la tarea
+    //Selecciona todos los campos de la tabla employee donde employeeid='[parametroQueLePasamos]'
+    Employee findByEmployeeid(String employeeid);
+}
